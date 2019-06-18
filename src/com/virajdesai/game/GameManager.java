968760a327ba75cf73f33799c5c5b3813240592e
Viraj Desai <virajpd19@gmail.com>
2019-06-18 16:35:02 -0400

    audio engine implementation

--- src/com/virajdesai/game/GameManager.java
+++ src/com/virajdesai/game/GameManager.java
@@ -3,23 +3,27 @@ package com.virajdesai.game;
 import com.virajdesai.engine.AbstractGame;
 import com.virajdesai.engine.GameContainer;
 import com.virajdesai.engine.Renderer;
+import com.virajdesai.engine.audio.SoundClip;
 import com.virajdesai.engine.gfx.Image;
 import com.virajdesai.engine.gfx.ImageTile;
 
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
 public class GameManager extends AbstractGame {
 
     private ImageTile image;
+    private SoundClip clip;
 
     public GameManager() {
         image = new ImageTile("/test.png", 16, 16);
+        clip = new SoundClip("/audio/clap.wav");
     }
 
     @Override
     public void update(GameContainer gc, float dt) {
-        if(gc.getInput().isKeyDown(KeyEvent.VK_A))
-            System.out.println("A was pressed");
+        if(gc.getInput().isButtonDown(MouseEvent.BUTTON1))
+            clip.play();
 
         temp += dt * 2;
 
